f65581ca577dba6a2c5b25d9dac47dcec0452b12
ruyga <44858604+ruyga@users.noreply.github.com>
2018-11-08 20:51:50 +0700

    Add files via upload

---
+++ candipuro.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+  class candipuro extends CI_Controller {
+    public function index()
+	{
+		$this->load->view('kecamatan/candipuro');
+	}
+}
